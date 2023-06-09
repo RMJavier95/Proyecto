6baff53da3eba92fb6e5ebe3f37e1e10d0b19eef
RMJavier95 <rmjavier10@gmail.com>
2023-06-09 11:25:38 +0200

    Terminado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,10 @@
                 <form method="POST" action="{{route('logout')}}">
                   @csrf
                   <button type="submit" class="text-sm mx-4 text-gray-900 dark:text-white hover:text-blue-700">
-                    Cerrar Sesión
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                    </svg>
+
                   </button>
                 </form>
                 
@@ -102,35 +105,36 @@
 
       </header>
 
-      <main class = "container mx-auto my-16">
-        <h2 class = "text-gray-900 text-center text-3xl font-bold mb-10">
-          @yield('titulo')
-        </h2>
-        @yield('contenido')
+      <div class="flex flex-col min-h-screen">
+        <main class = "flex-grow container mx-auto my-16">
+          <h2 class = "text-gray-900 text-center text-3xl font-bold mb-10">
+            @yield('titulo')
+          </h2>
+          @yield('contenido')
 
-      </main>
+        </main>
 
-      <footer class="bg-white shadow dark:bg-gray-900 bottom-0 inset-x-0 transform translate-y-full">
-        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-            <div class="sm:flex sm:items-center sm:justify-between">
-                <a href="/" class="flex items-center mb-4 sm:mb-0">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Biblioteca</span>
-                </a>
-                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                    <li>
-                        <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ now()->year }} <a href="/principal" class="hover:underline">Biblioteca™</a>. All Rights Reserved.</span>
-        </div>
-      </footer>
-
+        <footer class="mt-auto bg-white shadow dark:bg-gray-900 bottom-0 inset-x-0 transform translate-y-full">
+          <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+              <div class="sm:flex sm:items-center sm:justify-between">
+                  <a href="/" class="flex items-center mb-4 sm:mb-0">
+                      <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+                      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Biblioteca</span>
+                  </a>
+                  <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                      <li>
+                          <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                      </li>
+                      <li>
+                          <a href="#" class="hover:underline">Contact</a>
+                      </li>
+                  </ul>
+              </div>
+              <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+              <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ now()->year }} <a href="/principal" class="hover:underline">Biblioteca™</a>. All Rights Reserved.</span>
+          </div>
+        </footer>
+      </div>
 
       
   
